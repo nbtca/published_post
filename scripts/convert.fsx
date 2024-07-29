d80b10d7b377e56ae7409afc334638bb345be620
LazuliKao <LazuliKao233@outlook.com>
2024-07-29 22:06:53 +0800

    update script

--- scripts/convert.fsx
+++ scripts/convert.fsx
@@ -1,11 +1,15 @@
 #load "./src/docker.fsx"
+#load "./src/types.fsx"
+open Types
 open Docker
 open Docker.DotNet.Models
 open System.IO
+open System.Text
+open Newtonsoft.Json
 let cd = __SOURCE_DIRECTORY__
 let tempDir = Path.Combine(cd, "temp")
 Directory.CreateDirectory(tempDir) |> ignore
-let convert from saveTo = task {
+let rec convert from saveTo = async {
     let randomName = Path.ChangeExtension(Path.GetRandomFileName(), ".pdf")
     let randomOutput = Path.ChangeExtension(randomName, ".html")
     let tempFile = Path.Combine(tempDir, randomName)
@@ -15,11 +19,47 @@ let convert from saveTo = task {
         CreateContainerParameters(
             Image = "iise/pdf2htmlex-alpine",
             WorkingDir = "/pdf",
-            Cmd = [| "pdf2htmlEX"; "--zoom"; "1.3"; randomName; randomOutput |],
+            Cmd =
+                [| "pdf2htmlEX"
+                   "--zoom"
+                   "1"
+                   "--external-hint-tool=ttfautohint"
+                   //    "--embed=cfijo"
+                   randomName
+                   randomOutput |],
             HostConfig = HostConfig(Binds = [| $"{tempDir}:/pdf" |])
         )
-    do! connector.RunContainerOnceAsync(info)
+    do! connector.RunContainerOnceAsync(info) |> Async.AwaitTask
     let output = Path.Combine(tempDir, randomOutput)
     File.Move(output, saveTo, true)
     File.Delete(tempFile)
+    postProcess saveTo
 }
+and postProcess file =
+    let content = file |> File.ReadAllText
+    let replace regex (replacement: string) (text: string) =
+        RegularExpressions.Regex.Replace(text, regex, replacement)
+    let remove regex = replace regex ""
+    let result =
+        content
+        |> replace "margin: 13px auto;" "margin: auto;"
+        |> replace "@media print" "@media screen"
+        |> remove """<img alt="" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACA.*?/>"""
+        |> remove """#page-container.+\n.*9e9e9e.*\n.*\n.*(\s|\S)+?\}"""
+        |> replace "<title></title>" "<title>标题</title>"
+    File.WriteAllText(file, result)
+let dataDir = Path.Combine(cd |> Path.GetDirectoryName, "data")
+Directory.CreateDirectory dataDir |> ignore
+Directory.EnumerateDirectories dataDir
+|> Seq.iter (fun dir ->
+    printfn "%s" dir
+    let files = Directory.EnumerateFiles dir
+    let info =
+        files
+        |> Seq.find _.EndsWith(".json")
+        |> File.ReadAllText
+        |> JsonConvert.DeserializeObject<ArticleInfo>
+    let pdfFiles = files |> Seq.filter _.EndsWith(".pdf")
+    printfn "%A" info
+    printfn "%A" pdfFiles)
+// convert "sample.pdf" "sample.html" |> Async.RunSynchronously
--- scripts/src/docker.fsx
+++ scripts/src/docker.fsx
@@ -3,6 +3,7 @@ open Docker.DotNet
 open Docker.DotNet.Models
 open System
 open System.Runtime.InteropServices
+open System.Threading
 type DockerConnector() =
     let config =
         let isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
@@ -13,6 +14,31 @@ type DockerConnector() =
                 "unix:///var/run/docker.sock"
         new DockerClientConfiguration(Uri(url))
     let client = config.CreateClient()
+    member this.pipeLogs id =
+        let rec loop (stream: MultiplexedStream) = task {
+            let buffer = Array.zeroCreate<byte> 4096
+            let! read = stream.ReadOutputAsync(buffer, 0, buffer.Length, Unchecked.defaultof<CancellationToken>)
+            if read.EOF then
+                return ()
+            elif read.Count > 0 then
+                let output = Text.Encoding.UTF8.GetString(buffer, 0, read.Count)
+                match read.Target with
+                | MultiplexedStream.TargetStream.StandardIn -> Console.WriteLine("STDIN: " + output)
+                | MultiplexedStream.TargetStream.StandardOut -> Console.WriteLine(output)
+                | MultiplexedStream.TargetStream.StandardError -> Console.Error.WriteLine(output)
+                | _ -> ()
+            else
+                return! loop stream
+        }
+        task {
+            use! stream =
+                client.Containers.GetContainerLogsAsync(
+                    id,
+                    true,
+                    ContainerLogsParameters(ShowStdout = true, ShowStderr = true, Follow = true)
+                )
+            return! loop stream
+        }
     member _.Client = client
     interface IDisposable with
         member _.Dispose() =
@@ -24,6 +50,7 @@ type DockerConnector() =
         let! success = client.Containers.StartContainerAsync(id, null)
         if not success then
             return ()
+        this.pipeLogs id |> ignore
         let! _ = client.Containers.WaitContainerAsync(id)
         let! _ = client.Containers.RemoveContainerAsync(id, new ContainerRemoveParameters(Force = true))
         return ()
